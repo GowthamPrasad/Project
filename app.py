d80817e9bb95d569a71bea8e39cd9c978a4267f4
GowthamPrasad <gowthamsgp.11@gmail.com>
2022-05-25 22:03:24 +0530

    added email module

--- app.py
+++ app.py
@@ -2,13 +2,16 @@
 import numpy as np
 import string # FOR WORKING WITH STRINGS
 import random # FOR PASSWORD GENERATION
-from flask import Flask, request, redirect, url_for, jsonify, render_template # FOR FLASK AND RELATED FEATURES
+from flask import Flask, flash, request, redirect, url_for, jsonify, render_template # FOR FLASK AND RELATED FEATURES
 import pickle
 import speech_recognition as sr # GOOGLE SPEECH RECOGNITION API
 import datetime # FOR TIMESTAMP
 import hashlib # CALCULATING THE HASH IN ORDER TO ADD DIGITAL FINGERPRINTS TO THE BLOCKS
 import json # TO STORE DATA IN BLOCKCHAIN
 import sqlite3 as sql # TO STORE THE DATA
+import smtplib # TO SEND EMAIL
+from email.message import EmailMessage # TO SEND EMAIL
+
 
 
 # BLOCKCHAIN CLASS
@@ -72,7 +75,7 @@ class Blockchain:
 		
         return True
     
-# this function used to generate password
+# This Function used to generate password
 characters = list(string.ascii_letters + string.digits + "!@#$%^&*()")
 
 def generate_random_password():
@@ -96,6 +99,8 @@ def generate_random_password():
 
 # Flask app initialization
 app = Flask(__name__)
+app.secret_key = "super secret key"
+
 
 # Rendering Home page - index.html
 @app.route('/')
@@ -127,7 +132,7 @@ def transcribe():
             # using google speech recognition
             text = r.recognize_google(audio_text)
             print('Converting audio transcripts into text ...')
-            #print(text)
+            # print(text)
      
         except:
              print('Sorry...run again...')
@@ -139,21 +144,44 @@ def transcribe():
     # Insert patient name, password and transcribed text into 'mt.db' 
     user_name=None
     password=None
+    email=None
     password=generate_random_password() # Calling Password Generation function
     if request.method == 'POST':
         user_name=request.form['name']
+        email=request.form['email']
         con = sql.connect('mt.db')
         print("Connected successfully")
         cur = con.cursor()
-        cur.execute('INSERT INTO User(UserName,Password,File) VALUES (?,?,?)',(user_name,password,text))
+        cur.execute('INSERT INTO User(UserName,Password,File,Email) VALUES (?,?,?,?)',(user_name,password,text,email))
         con.commit()
         print("Data inserted successfully")
         con.close()
 
+
+    # Send password to patient email address
+    # Initialise EmailMessage()
+    msg = EmailMessage()
+    # message to be sent
+    msg.set_content('Your password to access the medical report: '+password)
+
+    msg['Subject'] = 'Password to access Medical Report | ABC Hospital'
+    msg['From'] = "gowthamprasads17mss018@skasc.ac.in"
+    msg['To'] = email
+
+    # creates SMTP session
+    server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+    # Authentication
+    server.login("gowthamprasads17mss018@skasc.ac.in", "good luck bro")
+    server.send_message(msg)
+    server.quit()
+    print("Password mailed successfully")
+
+
     return render_template('sample.html',transcribed_text=text)
     #return redirect(url_for('mine_block',text=text))
+
     
-        
+    
 # Rendering Access Document page - read.html
 @app.route('/read')
 def read():
@@ -182,6 +210,7 @@ def access():
         return render_template('read.html',transcribed_text=res)
     
     # else:
+    #     flash('Invalid password or username!')
     #     return render_template('read.html')
 
 
@@ -226,14 +255,6 @@ def valid():
     return jsonify(response), 200
     
     
-#@app.route('/')
-#def main():
-    # fo = open("test.txt","wb")
-    # fo.write("This is Test Data")
-    #with open("test.txt", "w") as fo:
-        #fo.write("This is Test Data")
-    
-    #return render_template('sample.html')
     
 # EXECUTION OF FLASK APP
 if __name__ == "__main__":
